7fd17852f8d844dc1b19808ea61bb8e62af27f5d
Sam Sulaimanov <sam@bitmorse.com>
2015-12-19 14:37:28 +0100

    adding new .nervous helloworld app

--- NervousMagicApps/NervousMagicApps/JSAppViewController.swift
+++ NervousMagicApps/NervousMagicApps/JSAppViewController.swift
@@ -16,19 +16,18 @@ class JSAppViewController: UIViewController {
     @IBOutlet weak var JSAppWebView: UIWebView!
     //Webviewcontroller scheme calls
     
+    override func viewWillAppear(animated: Bool) {
+        //hide status bar
+        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.Fade);
+    }
     
     
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
         //test webview
-        let url = NSURL(string: "http://localhost:8080/json"); //"https://sid027.github.io/Hackday-App/hello-world/app.html");
+        let url = NSURL(string: "https://sid027.github.io/Hackday-App/hello-world/app.html");
         let request = NSURLRequest(URL: url!);
         JSAppWebView.loadRequest(request);
         JSAppWebView.scrollView.bounces = false;
