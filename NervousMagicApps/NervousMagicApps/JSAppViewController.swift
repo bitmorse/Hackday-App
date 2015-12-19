c070d2531a02e36e39f3367ec394245d5db693d3
Sam Sulaimanov <sam@bitmorse.com>
2015-12-19 12:49:05 +0100

    changing submodule

--- NervousMagicApps/NervousMagicApps/JSAppViewController.swift
+++ NervousMagicApps/NervousMagicApps/JSAppViewController.swift
@@ -28,7 +28,7 @@ class JSAppViewController: UIViewController {
         
         
         //test webview
-        let url = NSURL(string: "http://sid027.github.io/Hackday-App/hello-world/app.html");
+        let url = NSURL(string: "https://sid027.github.io/Hackday-App/hello-world/app.html");
         let request = NSURLRequest(URL: url!);
         JSAppWebView.loadRequest(request);
         
