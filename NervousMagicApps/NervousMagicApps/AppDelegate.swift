5d822842e66ae053d3a8945ee06a2035ad0df6ce
sid <sid@wlan-dock-rz-1-0483.ethz.ch>
2015-12-19 13:49:19 +0100

    working http local server

--- NervousMagicApps/NervousMagicApps/AppDelegate.swift
+++ NervousMagicApps/NervousMagicApps/AppDelegate.swift
@@ -29,6 +29,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let jsonObject: NSDictionary = [NSString(string: "foo"): NSNumber(int: 3), NSString(string: "bar"): NSString(string: "baz")]
             return .OK(.Json(jsonObject))
         }
+        
+        server["/test/:param1/:param2"] = { r in
+            var headersInfo = ""
+            for (name, value) in r.headers {
+                headersInfo += "\(name) : \(value)<br>"
+            }
+            var queryParamsInfo = ""
+            for (name, value) in r.queryParams {
+                queryParamsInfo += "\(name) : \(value)<br>"
+            }
+            var pathParamsInfo = ""
+            for token in r.params {
+                pathParamsInfo += "\(token.0) : \(token.1)<br>"
+            }
+            return .OK(.Html("<h3>Address: \(r.address)</h3><h3>Url:</h3> \(r.url)<h3>Method: \(r.method)</h3><h3>Headers:</h3>\(headersInfo)<h3>Query:</h3>\(queryParamsInfo)<h3>Path params:</h3>\(pathParamsInfo)"))
+        }
 
         return true
     }
